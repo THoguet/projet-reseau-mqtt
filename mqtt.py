7284fd71a8563ddacbb48e10ec9c7f86dc68d8b4
Théophyl Hoguet <theophyl.hoguet@etu.u-bordeaux.fr>
2021-12-08 12:33:32 +0100

    debut c'est la merde

--- mqtt.py
+++ mqtt.py
@@ -12,44 +12,62 @@ PORT = 1883
 
 
 def create_mqtt_publish_msg(topic, value, retain=False):
-    """ create mqtt publish msg
-    >>> create_mqtt_publish_msg("temp","45")
-    b'0\\x08\\x00\\x04temp45'
-    """
-    tmp = 48
-    if retain:
-        tmp += 1
-    #on cherche la taille de topic et value
-    topic.encode("utf-8")
-    topic_length = len(topic)
-    value.encode("utf-8")
-    value_length = len(value)
-    #total
-    message_length = value_length + topic_length + 2
-    request = (tmp).to_bytes(1, byteorder="big") + (message_length).to_bytes(1, byteorder = 'big') + (topic_length).to_bytes(2, byteorder = 'big') + topic.encode("utf-8") + value.encode("utf-8")
-    return request
+	""" create mqtt publish msg
+	>>> create_mqtt_publish_msg("temp","45")
+	b'0\\x08\\x00\\x04temp45'
+	"""
+	tmp = 48
+	if retain:
+		tmp += 1
+	#on cherche la taille de topic et value
+	topic.encode("utf-8")
+	topic_length = len(topic)
+	value.encode("utf-8")
+	value_length = len(value)
+	#total
+	message_length = value_length + topic_length + 2
+	request = (tmp).to_bytes(1, byteorder="big") + (message_length).to_bytes(1, byteorder = 'big') + (topic_length).to_bytes(2, byteorder = 'big') + topic.encode("utf-8") + value.encode("utf-8")
+	return request
 
-print(create_mqtt_publish_msg("monsieur ", "théophyl").decode("utf-8"))
+# def decode_msg(msg):
+# 	msg
+
+print(create_mqtt_publish_msg("monsieur ", "théophyl")[0:1] == 0x30.to_bytes(1,byteorder="big"))
 
 def run_publisher(addr, topic, pub_id, retain=False):
-    """
-    Run client publisher.
-    """
-    pass
+	"""
+	Run client publisher.
+	"""
+	pass
 
 
 def run_subscriber(addr, topic, sub_id):
-    """
-    Run client subscriber.
-    """
-    pass
+	"""
+	Run client subscriber.
+	"""
+	pass
 
 
 def run_server(addr):
-    """
-    Run main server loop
-    """
-    pass
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.bind(addr)
+	s.listen(1)
+	l = [s]
+	while True:
+		l3, _, _ = select.select(l,[],[])
+		for i in l3:
+			if i == s:
+				conn, _ = i.accept()
+				l.append(conn)
+			else:
+				data = i.recv(1500)
+				if not data:
+					i.close()
+					l.remove(i)
+				else:	
+					print(data.decode('utf-8'))
+					i.sendall(data)
+	pass
 
 #!/usr/bin/python3
 # import socket
